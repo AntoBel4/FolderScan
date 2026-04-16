2344fc1c8607ff6cee6c107cd9b84ddb496bbc6f
AntoBel4 <estarellas.antoine@orange.fr>
2026-04-17 00:33:06 +0200

    chore: bootstrap repo with stubs and initial config

---
+++ src/cli/index.ts
@@ -0,0 +1,27 @@
+﻿// src/cli/index.ts
+import { Command } from "commander";
+const program = new Command();
+
+program
+  .name("folderscope")
+  .description("FolderScope AI - CLI")
+  .version("0.1.0");
+
+program
+  .command("scan <path>")
+  .description("Scan a folder and generate reports")
+  .option("--dry-run", "Do not write files")
+  .action((path, opts) => {
+    console.log("Stub: scan", path, opts);
+    process.exit(0);
+  });
+
+program
+  .command("report")
+  .description("Generate global report")
+  .action(() => {
+    console.log("Stub: report");
+    process.exit(0);
+  });
+
+program.parse(process.argv);
---
+++ src/core/scanner.ts
@@ -0,0 +1,14 @@
+﻿// src/core/scanner.ts
+import * as fs from "fs";
+import * as path from "path";
+
+export async function scanFolder(root: string) {
+  const entries = fs.readdirSync(root, { withFileTypes: true });
+  const files = entries.map(e => ({
+    name: e.name,
+    isDir: e.isDirectory()
+  }));
+  return { root, files };
+}
+
+export default scanFolder;
